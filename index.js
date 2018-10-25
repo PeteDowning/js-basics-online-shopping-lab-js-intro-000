77171117c30f8abe3a29e66f1d8f7c38df420a8c
Peter Downing <petedowning14@gmail.com>
2018-10-25 10:53:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ function addToCart(item) {
 function viewCart() {
   // write your code here
   if (cart.length > 0) {
-    print(cart)
     var string = "In your cart, you have "
     for (var i = 0; i < cart.length; i++) {
       // var object = cart[i]
