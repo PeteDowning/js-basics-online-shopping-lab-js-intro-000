c23e1b20ccbf6aaeb71ac8e9157d61964960d167
Peter Downing <petedowning14@gmail.com>
2018-10-25 10:37:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,6 +71,6 @@ function placeOrder(cardNumber) {
   } else {
     var price = total()
     cart = [];
-    return "Your total cost is \$$(price), which will be charged to the card $(cardNumber)."
+    return "Your total cost is $" + price + ", which will be charged to the card " + cardNumber + "."
   }
 }
