313f241f40b07682308084aa063a221d560c6d08
Peter Downing <petedowning14@gmail.com>
2018-10-25 10:59:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
-  if (cart.length > 0) {
+  if (cart.length > 1) {
     var string = "In your cart, you have "
     for (var i = 0; i < cart.length; i++) {
       // var object = cart[i]
@@ -31,6 +31,8 @@ function viewCart() {
         string = string.concat("and " + name + " at $" + price + ".")
       }
     }
+  } else if (cart.length == 1) {
+    
   } else {
     return "Your shopping cart is empty."
   }
