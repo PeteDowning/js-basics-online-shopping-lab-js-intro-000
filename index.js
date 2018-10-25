9e14f91964be01c60a053370603cf604ae18a80b
Peter Downing <petedowning14@gmail.com>
2018-10-25 09:59:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,8 +20,9 @@ function addToCart(item) {
 function viewCart() {
   // write your code here
   if (cart.length > 0) {
-    for (var i = 0; i < array.length; i++) {
-      array[i]
+    var string = "In your cart, you have "
+    for (var i = 0; i < cart.length; i++) {
+      string = string.concat()
     }
   } else {
     return "Your shopping cart is empty."
